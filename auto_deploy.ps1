3c75edb5e386fa5cdbcec7c9a2b81d04ecbed12a
codehippie1 <codehippie1@gmail.com>
2019-05-24 14:59:32 -0400

    minor changes on version# and samples

--- auto_deploy.ps1
+++ auto_deploy.ps1
@@ -1,8 +1,8 @@
 npm run build
 cd dist
 npm pack
-copy .\ng2-pdfjs-viewer-1.0.0.tgz ..\ng6SampleApp\ -Force
+copy .\ng2-pdfjs-viewer-3.2.0.tgz ..\ng6SampleApp\ -Force
 cd ..\ng6SampleApp\
 npm uninstall ng2-pdfjs-viewer
-npm install .\ng2-pdfjs-viewer-1.0.0.tgz
+npm install .\ng2-pdfjs-viewer-3.2.0.tgz
 ng serve
