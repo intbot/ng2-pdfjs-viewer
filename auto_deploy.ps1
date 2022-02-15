8563a89ef59d8590e77c54b6a5eb08e809b646aa
Code Hippie <codehippie1@gmail.com>
2022-02-15 15:05:24 -0500

    + Moving to ng-packager + Moving to angular 13 peer dependancy

--- auto_deploy.ps1
+++ auto_deploy.ps1
@@ -1,9 +1,9 @@
-copy .\pdfjs\web\viewer.js .\SampleApp\src\assets\pdfjs\web -Force
+copy .\pdfjs\web\viewer.js ..\SampleApp\src\assets\pdfjs\web -Force
 npm run build
 cd dist
 npm pack
-copy .\ng2-pdfjs-viewer-6.0.1.tgz ..\SampleApp\ -Force
-cd ..\SampleApp\
+copy .\ng2-pdfjs-viewer-13.2.2.tgz ..\..\SampleApp\ -Force
+cd ..\..\SampleApp\
 npm uninstall ng2-pdfjs-viewer
-npm install .\ng2-pdfjs-viewer-6.0.1.tgz
-ng serve
+npm install .\ng2-pdfjs-viewer-13.2.2.tgz
+npm start
---
+++ dist/esm2020/ng2-pdfjs-viewer.mjs
@@ -0,0 +1,5 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './index';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmcyLXBkZmpzLXZpZXdlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL25nMi1wZGZqcy12aWV3ZXIudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0dBRUc7QUFFSCxjQUFjLFNBQVMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogR2VuZXJhdGVkIGJ1bmRsZSBpbmRleC4gRG8gbm90IGVkaXQuXG4gKi9cblxuZXhwb3J0ICogZnJvbSAnLi9pbmRleCc7XG4iXX0=
