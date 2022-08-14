0e512edf2ba3ea86d67f1162cbb23e6aa138665d
Derek Burgman <derek@dereekb.com>
2022-08-13 22:35:18 -0500

    build: angular 14

--- index.ts
+++ index.ts
@@ -1,24 +1,2 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PdfJsViewerComponent } from './src/ng2-pdfjs-viewer.component';
-
 export * from './src/ng2-pdfjs-viewer.component';
-
-@NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [
-    PdfJsViewerComponent
-  ],
-  exports: [
-    PdfJsViewerComponent
-  ]
-})
-export class PdfJsViewerModule {
-  static forRoot(): ModuleWithProviders<PdfJsViewerModule> {
-    return {
-      ngModule: PdfJsViewerModule
-    };
-  }
-}
+export * from './src/ng2-pdfjs-viewer.module';
