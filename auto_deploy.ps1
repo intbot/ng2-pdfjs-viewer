f70903e77a0848243a883faf83b74801c0c94632
Aneesh <codehippie1@gmail.com>
2022-11-30 20:15:43 -0500

    Changes in folder structure

--- auto_deploy.ps1
+++
@@ -1,9 +0,0 @@
-copy .\pdfjs\web\viewer.js ..\SampleApp\src\assets\pdfjs\web -Force
-npm run build
-cd dist
-npm pack
-copy .\ng2-pdfjs-viewer-14.0.0.tgz ..\..\SampleApp\ -Force
-cd ..\..\SampleApp\
-npm uninstall ng2-pdfjs-viewer
-npm install .\ng2-pdfjs-viewer-14.0.0.tgz
-npm start
